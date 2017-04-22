5dab9eee0a6a5e1a13f81115a5ae09b4e3488997
Batanayi Matuku <bdizha@gmail.com>
2017-04-22 20:30:26 +0200

    Modified: carousel elements

--- local/resources/views/search/index.blade.php
+++ local/resources/views/search/index.blade.php
@@ -67,8 +67,12 @@
                     </div>
                 </div>
                 <div class="col-sm-3 col-xs-12">
-                    <button type="submit" class="btn btn-default btn-lg btn-block">Go</button>
-                    <a href="/local/search?all=true" class="btn btn-primary btn-lg btn-block">BROWSE ALL</a>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-default btn-lg btn-block">Go</button>
+                    </div>
+                    <div class="form-group">
+                        <a href="/local/search?all=true" class="btn btn-primary btn-lg btn-block">BROWSE ALL</a>
+                    </div>
                     <!--<a href="#" class="btn btn-primary btn-lg btn-block" id="advanced-search">ADVANCED SEARCH</a>-->
                 </div>
             </div>
