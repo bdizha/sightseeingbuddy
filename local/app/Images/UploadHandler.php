c0933ae200e0f384b3398c8709f6bccddf2fe19c
Batanayi Matuku <bdizha@gmail.com>
2017-04-24 19:41:08 +0200

    Modified: upload handler

--- local/app/Images/UploadHandler.php
+++ local/app/Images/UploadHandler.php
@@ -519,12 +519,14 @@ class UploadHandler {
         if (count($nameParts) > 0) {
             $name = str_slug($nameParts[0]);
         }
+
+        $name = $name . "_" . time();
         
         if (count($nameParts) > 1) {
             $name .= "." . $nameParts[count($nameParts) - 1];
         }
         
-        return $name . "_" . time();
+        return $name;
     }
 
     protected function get_file_name($file_path, $name, $size, $type, $error, $index, $content_range) {
