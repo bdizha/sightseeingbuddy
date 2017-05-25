84538a68fa1afdc5cb933095b4a806d036fa6d1b
Batanayi Matuku <bdizha@gmail.com>
2017-05-25 23:07:17 +0200

    Added: social media handles

--- local/resources/views/partials/footer.blade.php
+++ local/resources/views/partials/footer.blade.php
@@ -5,12 +5,13 @@
             <div class="col-md-4">
                 <h2>Connect with us</h2>
                 <div class="socials-block">
-                    <a class="share-btn external-link" target="_blank" href="https://www.facebook.com/sightseeingbuddy.com"><i class="fa fa-facebook"></i></a>
-                    <a class="share-btn external-link" target="_blank" href="https://twitter.com/KeepitLocalSA"><i class="fa fa-twitter"></i></a>
-                    <a class="share-btn"><i class="fa fa-skype"></i></a>
+                    <a class="share-btn external-link" target="_blank" href="https://www.facebook.com/Sightseeing-Buddy-629528153904966/"><i class="fa fa-facebook"></i></a>
+                    <a class="share-btn external-link" target="_blank" href="https://twitter.com/SightBud"><i class="fa fa-twitter"></i></a>
+                    <a class="share-btn external-link" target="_blank" href="https://www.instagram.com/sightseeingbuddy/"><i class="fa fa-instagram"></i></a>
                     <a class="share-btn" target="_blank" href=""><i class="fa fa-youtube"></i></a>
                     <a class="share-btn" target="_blank" href=""><i class="fa fa-google-plus"></i></a>
-                    <a class="share-btn external-link" target="_blank" href="https://za.pinterest.com/keepitlocal0665"><i class="fa fa-pinterest"></i></a>
+                    <a class="share-btn" target="_blank" href=""><i class="fa fa-pinterest"></i></a>
+                    <a class="share-btn"><i class="fa fa-skype"></i></a>
                 </div>
                 <h2>Sign up to our newsletter</h2>
                 <div id="newsletter-group" class="form-group">
