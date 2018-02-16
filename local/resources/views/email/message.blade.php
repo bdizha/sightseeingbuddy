a99a6e1a0766b3863e458fa54df8b9a43e801dac
Batanayi Matuku <bdizha@gmail.com>
2018-02-16 23:07:11 +0200

    Modified: messages

---
+++ local/resources/views/email/message.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.email')
+@section('content')
+    <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tbody>
+        @include('email.partials.content', ['text' => $message->content])
+        </tbody>
+    </table>
+@stop
