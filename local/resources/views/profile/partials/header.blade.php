e936aad8ad143853b368aa140508d7d0328e48b2
Batanayi Matuku <bdizha@gmail.com>
2018-02-20 00:00:34 +0200

    Modified: profile changes

--- local/resources/views/profile/partials/header.blade.php
+++ local/resources/views/profile/partials/header.blade.php
@@ -1,9 +1,7 @@
 <div class="container profile">
 	<div class='row'>
         <div class="col-sm-3 col-xs-12 profile-picture img-1">
-            @if(!empty($user->id))
-              @include('booking.partials.user', ['user' => $user])
-            @endif
+            @include('booking.partials.user', ['user' => $user])
         </div>
         <div class="col-sm-9 col-xs-12">
             <div class='row mt-1'>
