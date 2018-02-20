35e0a6137582d800fee7bcc2cccced9476a02190
Batanayi Matuku <bdizha@gmail.com>
2018-02-20 02:01:48 +0200

    Modified: profile header

--- local/resources/views/profile/partials/header.blade.php
+++ local/resources/views/profile/partials/header.blade.php
@@ -1,13 +1,10 @@
 <div class="container profile">
-	<div class='row'>
-        <div class="col-sm-3 col-xs-12 profile-picture img-1">
-            @include('booking.partials.user', ['user' => $user])
-        </div>
-        <div class="col-sm-9 col-xs-12">
-            <div class='row mt-1'>
-    		@if(!empty($user->id))
-               	  <h1>HELLO {{ $user->first_name }}</h1>
-	        @endif
+    <div class='row'>
+        <div class="col-sm-12">
+            <div class='row mt-1 text-center'>
+                @if(!empty($user->id))
+                    <h1 class="text-center">HELLO {{ $user->first_name }}</h1>
+                @endif
                 <h2>{{ $title }}</h2>
             </div>
         </div>
