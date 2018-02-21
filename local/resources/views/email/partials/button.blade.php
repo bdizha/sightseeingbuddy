2ff8e273447356584e3b631d92bafe440c90bd4a
Batanayi Matuku <bdizha@gmail.com>
2018-02-21 18:29:12 +0200

    Added: 'Contact host' button in success email

--- local/resources/views/email/partials/button.blade.php
+++ local/resources/views/email/partials/button.blade.php
@@ -1,5 +1,5 @@
 <tr>
-    <td style="padding-top: 20px;padding-bottom: 20px;">
+    <td style="padding-top: 0;padding-bottom: 20px;">
         <a href="{{ url($url)}}" style="display: inline-block;color: #FFFFFF;text-decoration:none;font-weight: 500;font-size: 14px;padding: 10px;text-transform: uppercase;background: #1a75bb;border-color: #1a75bb;border-radius: 4px;-moz-box-shadow: 0 2px 1px 0 #1a75bb;-webkit-box-shadow: 0 2px 1px 0 #1a75bb;box-shadow: 0 2px 1px 0 #1a75bb;" target="_blank">
             {{ $text }}
         </a>
