93cb3df66806aa0ced1b7c2b542f83e060aadcf3
Batanayi Matuku <bdizha@gmail.com>
2018-02-24 15:17:35 +0200

    Modified: profile tabs order

--- local/resources/views/profile/partials/tabs.blade.php
+++ local/resources/views/profile/partials/tabs.blade.php
@@ -18,16 +18,16 @@
                     <span>Find my booking</span>
                 </a>
             </li>
-            <li class="item<?php echo $tab === 'dashboard' ? ' active' : "" ?>">
-                <a href="{{ "/local/dashboard" }}">
-                    <i class="dashboard"></i>
-                    <span>Dashboard</span>
-                </a>
-            </li>
             <li class="item<?php echo $tab === 'bookings' ? " active" : "" ?>">
                 <a href="{{ "/local/bookings" }}">
                     <i class="bookings"></i>
                     <span>Manage Booking</span>
+                </a>
+            </li>
+            <li class="item<?php echo $tab === 'dashboard' ? ' active' : "" ?>">
+                <a href="{{ "/local/dashboard" }}">
+                    <i class="dashboard"></i>
+                    <span>Dashboard</span>
                 </a>
             </li>
         </ul>
