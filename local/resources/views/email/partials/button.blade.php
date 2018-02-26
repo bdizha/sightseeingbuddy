21fb7f7fd1fe9783f3058c452e5ad857901e58cd
Batanayi Matuku <bdizha@gmail.com>
2018-02-26 20:27:32 +0200

    Modified: email styles

--- local/resources/views/email/partials/button.blade.php
+++ local/resources/views/email/partials/button.blade.php
@@ -1,12 +1,12 @@
 <tr>
-    <td style="padding-top: 0;padding-bottom: 20px;">
+    <td style="padding-top: 20px;padding-bottom: 0;">
         <!--[if mso]>
         <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://litmus.com" style="height:40px;line-height: 40px;v-text-anchor:middle;width:auto;" arcsize="5%" strokecolor="#1a75bb" fillcolor="#1a75bb">
             <w:anchorlock/>
-            <center style="color: #FFFFFF;text-decoration:none;font-weight: 500;font-size: 16px;padding: 10px;text-transform: uppercase;background: #1a75bb;border-color: #1a75bb;border-radius: 4px;-moz-box-shadow: 0 2px 1px 0 #1a75bb;-webkit-box-shadow: 0 2px 1px 0 #1a75bb;box-shadow: 0 2px 1px 0 #1a75bb;"> {{ $text }}</center>
+            <center style="color: #FFFFFF;text-decoration:none;font-weight: 500;font-size: 16px;padding: 5px 20px;text-transform: uppercase;background: #1a75bb;border-color: #1a75bb;border-radius: 4px;-moz-box-shadow: 0 2px 1px 0 #1a75bb;-webkit-box-shadow: 0 2px 1px 0 #1a75bb;box-shadow: 0 2px 1px 0 #1a75bb;"> {{ $text }}</center>
         </v:roundrect>
         <![endif]-->
-        <a href="{{ url($url)}}" style="-webkit-text-size-adjust:none;mso-hide:all;height:40px;line-height: 40px;display: inline-block;color: #FFFFFF;text-decoration:none;font-weight: 500;font-size: 16px;padding: 10px;text-transform: uppercase;background: #1a75bb;border-color: #1a75bb;border-radius: 4px;-moz-box-shadow: 0 2px 1px 0 #1a75bb;-webkit-box-shadow: 0 2px 1px 0 #1a75bb;box-shadow: 0 2px 1px 0 #1a75bb;" target="_blank">
+        <a href="{{ url($url)}}" style="-webkit-text-size-adjust:none;mso-hide:all;height:40px;line-height: 40px;display: inline-block;color: #FFFFFF;text-decoration:none;font-weight: 500;font-size: 16px;padding: 5px 20px;text-transform: uppercase;background: #1a75bb;border-color: #1a75bb;border-radius: 4px;-moz-box-shadow: 0 2px 1px 0 #1a75bb;-webkit-box-shadow: 0 2px 1px 0 #1a75bb;box-shadow: 0 2px 1px 0 #1a75bb;" target="_blank">
             {{ $text }}
         </a>
     </td>
