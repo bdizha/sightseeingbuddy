bd701289282e9ca19f3d9684602799e59be854ed
Batanayi Matuku <bdizha@gmail.com>
2018-03-09 12:03:45 +0200

    Modified: sign up welcome page

--- local/resources/views/auth/become.blade.php
+++ local/resources/views/auth/become.blade.php
@@ -7,13 +7,15 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Experience the Culture with Sightseeing Buddy</div>
                 <div class="panel-body">
-                    <p>If you are looking to book an authentic experience with a buddy or interested in hosting one, please sign up by selecting the button below</p>
+                    <p>If you are looking to book an authentic experience with a buddy or interested in hosting one,
+                        please sign up by selecting the button below</p>
                     <div class="row form-group">
                         <div class="col-sm-5 col-xs-12">
                             <a href="{{ route('register') }}" class="btn fullwidth btn-default">Find a buddy</a>
                         </div>
                         <div class="col-sm-4 col-xs-12">
-                            <a href="{{ url('/local/password/reset') }}" class="btn fullwidth btn-default">Become a buddy</a>
+                            <a href="{{ route("introduction.create") }}" class="btn fullwidth btn-default">Become a
+                                buddy</a>
                         </div>
                     </div>
                 </div>
